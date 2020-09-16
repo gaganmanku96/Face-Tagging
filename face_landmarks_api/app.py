e2ec66fc620d9a05d02f4e578b4a0c2549383006
gagan <gaganmanku96@gmail.com>
2020-09-16 23:27:29 +0530

    changes

--- face_landmarks_api/app.py
+++ face_landmarks_api/app.py
@@ -1,23 +1,22 @@
 from flask import Flask, request, jsonify
 import numpy as np
-import json
 from PIL import Image
 import face_recognizition_api as api
+import logging
 
 app = Flask(__name__)
 
 
 @app.route('/getkeypts', methods=['POST'])
 def get_keypts():
-    result = "error"
     try:
         image = request.files['file']
-        print("File recieved")
+        logging.info("Image recieved")
         image = np.array(Image.open(image))
         result = api.get_encoding(image, type='image')
-    except Exception as e:
-        print("Error in docker ")
-    return json.dumps(result)
+        return result
+    except Exception:
+        return "Something went wrong", 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0') 
+    app.run(debug=False, host='0.0.0.0') 
